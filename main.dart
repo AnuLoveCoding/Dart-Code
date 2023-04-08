d521d374dc83d3b52d339f06f393bfafc4d2f5af
anuLoveCoading <anusrivas07@gmail.com>
2023-04-08 15:48:38 +0530

    dart_project

--- main.dart
+++ main.dart
@@ -2,4 +2,20 @@ void main(List<String> args) {
   print('Anurag');
 
   print("Hello Anurag");
+
+  String name = 'Anurag';
+
+  print(name);
+
+  int value = 15;
+  print(value);
+
+  int classs = 150;
+  print(classs);
+
+
+  String name2 = 'Anurag Mundra';
+  print(name2);
+  print(name2.length);
+  print(name2.substring(0,3));
 }
