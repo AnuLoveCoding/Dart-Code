600995a796a16fcf12d38db7b56356f42be3f435
anuLoveCoading <anusrivas07@gmail.com>
2023-04-08 12:05:57 +0530

    dart_code

---
+++ class_01.dart
@@ -0,0 +1,53 @@
+// void main(){
+//   print('Anurag');
+//   print('Mundra');
+// //   print('Hello gyes');
+// //   print('Hello world');
+// }
+
+
+// variable name in dart 
+
+/*
+ * int
+ * Double
+ * String
+ * List
+ * Map
+ * Dynamic
+ * Boolean
+ */
+
+void main(){
+  String name1 = 'Anurag';
+  print(name1);
+  
+  int value = 100000;
+  print(value);
+  
+  double b = 100.526;
+  print(b);
+  
+  double c = 1.25;
+    print(c);
+  
+  
+  bool v = true;
+  print(v);
+  
+  
+  bool v1 = false;
+  print(v1);
+  
+  
+  List arr = [1,2,3,4,5,6,8,7,8,9];
+  print(arr[3]);
+  
+  
+  Map list = {'name' : 'Anurag', 'city' : 'Delhi', 'phone_number' : 8595848097};
+  print(list['name']);
+  
+  
+//   var is use for dynamically it is used for automatically understand the data types of variable.
+    
+}
