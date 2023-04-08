16fbe8f6710b934b52420f9ad8884ff05212ecb0
anuLoveCoading <anusrivas07@gmail.com>
2023-04-09 03:45:36 +0530

    dart_practise

--- operators.dart
+++ operators.dart
@@ -10,47 +10,73 @@ import 'dart:ffi';
 
 void main(List<String> args) {
   // ! var data-type is used for dynamic;
-  var a = 10;
-  var b = 20;
+  // var a = 10;
+  // var b = 20;
 
-  a = 30;
+  // a = 30;
 
-  print(a + b);
-  print(a - b);
-  print(a * b);
-  print(a / b);
-  print(a % b);
-  print(a ~/ b);
+  // print(a + b);
+  // print(a - b);
+  // print(a * b);
+  // print(a / b);
+  // print(a % b);
+  // print(a ~/ b);
 
   // ! Relational operators (<,>, >=, <=, ==, !)
 
-  print(a < b);
-  print(a <= b);
-  print(a == b);
-  print(a != b);
-  print(a >= b);
-  print(a > b);
+  // print(a < b);
+  // print(a <= b);
+  // print(a == b);
+  // print(a != b);
+  // print(a >= b);
+  // print(a > b);
 
   // ?
   // ?
 
   // ! Test operators (is, is!);
 
-  print(a is String);
-  print(a is Int);
-  print(a is bool);
-  print(b is! bool);
+  // print(a is String);
+  // print(a is Int);
+  // print(a is bool);
+  // print(b is! bool);
 
   // ! Increment and Decrement (++, --)
 
-  int c = 15;
-  print(c++); // ! post Increment;
-  print(++c); // ! pre Increment;
-  print(c--); // ? post Dcrement;
-  print(--c); // ? pre Dcrement;
+  // int c = 15;
+  // print(c++); // ! post Increment;
+  // print(++c); // ! pre Increment;
+  // print(c--); // ? post Dcrement;
+  // print(--c); // ? pre Dcrement;
 
-  int u = 10;
-  u += 20;
+  // int u = 10;
+  // u += 20;
 
-  print(u);
+  // print(u);
+
+  // ! Logical Operators ( &&, ||, !);
+
+  // var l = 50;
+  // var m = 25;
+
+  // if (l == 60 && m != 25) {
+  //   print(true);
+  // } else {
+  //   print(false);
+  // }
+
+  // ! conditional ?: ??
+
+  // var a = 10;
+  // print(a > 20 ? 'A is greater than 10' : 'A is less than 10');
+
+  // ! Cascading Notations (..);
+
+  var str = 'Anurag';
+
+  print(str..toLowerCase()..substring(2));
+
+  // ! Bitwise Operators ( &, |, `, ^, a<<b, a>>b);
+
+  
 }
