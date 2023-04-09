26de38962fdff55f88ffd1b8d4f88c887e007d18
anuLoveCoading <anusrivas07@gmail.com>
2023-04-09 18:28:42 +0530

    dart_practise

---
+++ object.dart
@@ -0,0 +1,21 @@
+// ? What is OOP's
+// ! class
+// ! object
+// * Constructor
+  // ! Default constructor
+  // ! par constructor
+  // ! Named constructor
+
+
+void main(List<String> args) {
+  var obj = class1();
+  obj.func1();
+  print(obj.name);
+}
+
+class class1 {
+  var name = 'Anurag';
+  func1() {
+    print('Hello ANUrag');
+  }
+}
