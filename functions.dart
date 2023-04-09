8526c56d670c42e570f55b0fbebea0f4c3cd8149
anuLoveCoading <anusrivas07@gmail.com>
2023-04-09 15:11:06 +0530

    dart_practise

--- functions.dart
+++ functions.dart
@@ -1,5 +1,17 @@
 void main(List<String> args) {
   // ! functions in dart language;
+  // ! functions rules;
+  // ! functions expression - FAT Arrow;
+  // ! parameters;
+  // ! Optional parameters;
+  // ! Named Parameters;
 
+  var x = getNumber(10, 20);
+  print(x * 2);
   
+}
+
+int getNumber(int a, int b) {
+  var y = (a * b) + 10;
+  return (y);
 }
